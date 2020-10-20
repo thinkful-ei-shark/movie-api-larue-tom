39eb2e38842cb327b9684a55564ca183d14c12db
lrny-lru <60946728+lrny-lru@users.noreply.github.com>
2020-10-20 17:30:15 -0400

    add dotenv

--- src/server.js
+++ src/server.js
@@ -5,36 +5,44 @@ const helmet = require('helmet');
 const cors = require('cors');
 const movies = require('./movielibrary.json');
 const app = express();
-app.use(morgan('dev'));
+app.use(morgan('dev') );
 
-app.use(function )
+app.use(function validateBearerToken(req, res, next){
+  const apitoken = process.env.API_TOKEN;
+
+  const authToken = req.get('Authorizon');
+  if(!authToken || authToken.split(' ')[1] !== apiToken){
+    return res.status(401).json({ error: 'Unauthorized request'});
+  }
+  next();
+} );
 
 
 
 app.get('/movie', function getMovies (req, res)  {
   let reply = movies;
   if(req.query.genre) {
-      reply = reply.filter(film =>
-        film.genre.toLowerCase()
-        .includes(req.query.genre.toLowerCase())
-      )
-    }
+    reply = reply.filter(film =>
+      film.genre.toLowerCase()
+        .includes(req.query.genre.toLowerCase() )
+    );
+  }
 
-    if(req.query.country) {
-        reply = reply.filter( film => 
-            film.country.toLowerCase()
-            .includes(req.query.country.toLowerCase())
-        )
-    }
-    if (req.query.avg_vote){
-        reply = rely.filter(film => 
-            Number(movie.avg_vote) >= Number(req.query.avg_vote)
-            )
-    }
-    res.json(reply);
-const PORT = 8000
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
-})
+  if(req.query.country) {
+    reply = reply.filter( film => 
+      film.country.toLowerCase()
+        .includes(req.query.country.toLowerCase())
+    );
+  }
+  if (req.query.avg_vote){
+    reply = rely.filter(film => 
+      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
+  res.json(reply);
+  const PORT = 8000;
+  app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+  });
 
 });
