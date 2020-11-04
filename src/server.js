66d3d799383b91757a84f7b158611b8ba4a4d7b8
lrny-lru <60946728+lrny-lru@users.noreply.github.com>
2020-11-04 14:45:40 -0500

    this works

--- src/server.js
+++ src/server.js
@@ -15,44 +15,30 @@ app.use(helmet());
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
 
-  const authToken = req.get('Authorizon');
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    return res.status(401).json({ error: 'Unauthorized request' });
-  }
+  const authToken = req.get('Authorization');
+
+  if ( !authToken || authToken.split(' ')[1] !== apiToken ) return res.status(401).json( { error: 'Unauthorized request' } );
+  
   next();
-});
+}); 
 
-
-
-app.get('/movie', function getMovies(req, res) {
+app.get('/movie', function handleGetMovie( req, res ){
   let reply = movies;
-  if (req.query.genre) {
-    reply = reply.filter(film =>
-      film.genre.toLowerCase()
-        .includes(req.query.genre.toLowerCase())
-    );
-  }
+  const ask = req.query;
 
-  if (req.query.country) {
-    reply = reply.filter(film =>
-      film.country.toLowerCase()
-        .includes(req.query.country.toLowerCase())
-    );
-  }
-<<<<<<< HEAD
-  if (req.query.avg_vote){
-    reply = reply.filter(film => 
-=======
-  if (req.query.avg_vote) {
-    reply = reply.filter(film =>
->>>>>>> a28988f1b0651cab71725476a4bb690e0c487128
-      Number(film.avg_vote) >= Number(req.query.avg_vote)
-    );
-  }
-  res.json(reply);
-  const PORT = 8000;
-  app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`);
-  });
+  if( ask.genre ) reply = reply.filter( film => film.genre.toLowerCase( ).includes( ask.genre.toLowerCase( ) ) );
 
+  if( ask.country ) reply = reply.filter( film => film.country.toLowerCase( ).includes( ask.country.toLowerCase( ) ) );
+
+  if( ask.avg_vote ) reply = reply.filter( film => Number( film.avg_vote ) >= Number( ask.avg_vote ) );
+
+  res.json( reply );
+
+} );
+
+
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
